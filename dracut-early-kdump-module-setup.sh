ed9a5e62d8db5f9dd4027530efe97087a61fb543
Kazuhito Hagio <k-hagio@ab.jp.nec.com>
2018-05-02 09:38:57 -0400

    early-lianbo.v4

---
+++ dracut-early-kdump-module-setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+. /etc/sysconfig/kdump
+. /lib/kdump/kdump-lib.sh
+
+KDUMP_KERNEL=""
+KDUMP_INITRD=""
+
+check() {
+    if [ ! -f /etc/sysconfig/kdump ] || [ ! -f /lib/kdump/kdump-lib.sh ]\
+        || [ -n "${IN_KDUMP}" ]
+    then
+        return 1
+    fi
+    return 255
+}
+
+depends() {
+    echo "base shutdown"
+    return 0
+}
+
+prepare_kernel_initrd() {
+        KDUMP_BOOTDIR=$(check_boot_dir "${KDUMP_BOOTDIR}")
+        if [ -z "$KDUMP_KERNELVER" ]; then
+                kdump_kver=`uname -r`
+        else
+                kdump_kver=$KDUMP_KERNELVER
+        fi
+        KDUMP_KERNEL="${KDUMP_BOOTDIR}/${KDUMP_IMG}-${kdump_kver}${KDUMP_IMG_EXT}"
+        KDUMP_INITRD="${KDUMP_BOOTDIR}/initramfs-${kdump_kver}kdump.img"
+}
+
+install() {
+    inst_simple "/etc/sysconfig/kdump" "/etc/sysconfig/kdump"
+    inst_simple "/usr/sbin/kexec" "/usr/sbin/kexec"
+    inst_script "/lib/kdump/kdump-lib.sh" "/lib/kdump-lib.sh"
+    inst_hook cmdline 00 "$moddir/early-kdump.sh"
+    prepare_kernel_initrd
+    inst_simple "$KDUMP_KERNEL"
+    inst_simple "$KDUMP_INITRD"
+}
